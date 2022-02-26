13767bea8778f3919367bfd455cf4d87cd0dc3ff
amirmansouri <amir27069149@gmail.com>
2022-02-26 19:37:21 +0100

    isa

---
+++ app/src/main/java/Login/Login.kt
@@ -0,0 +1,13 @@
+package Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isa_mobile.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+    }
+}
---
+++ app/src/main/java/nav_bottom/nav_bottom.kt
@@ -0,0 +1,12 @@
+package nav_bottom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isa_mobile.R
+
+class nav_bottom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_bottom)
+    }
+}
